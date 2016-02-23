98be3ab544f4d6863b138397fce52862c80ccc37
Gilbert Pellegrom <gilbert@pellegrom.me>
2016-02-23 10:12:47 +0000

    Add global AJAX timeout

--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -8,3 +8,7 @@ App.Util.calcLocalTime = function(tzOffset) {
 	var utc = new Date(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate(), d.getUTCHours(), d.getUTCMinutes(), d.getUTCSeconds());
 	return new Date(utc.getTime() + (tzOffset * 1000));
 };
+
+$.ajaxSetup({
+	timeout: 30000
+});
